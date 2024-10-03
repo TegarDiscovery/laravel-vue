fd2554adbd73251535f63665ca9a341f3835da4f
Tegar Penemuan <tegarpenemuan0@gmail.com>
2024-10-03 12:55:12 +0700

    Build Vue

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    build: {
+        outDir: 'dist', // Ini adalah direktori output default
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
